7eb1b03ce118ef6f57ffd238a589029eae5a9ec6
Marcos Gonalons <m.gonalons.camps@gmail.com>
2021-07-06 18:47:53 +0200

    Moved some methods from ibroker api to other file to make it more clean

---
+++ src/services/api/ibroker/boolean_checks.go
@@ -0,0 +1,81 @@
+package ibroker
+
+import (
+	"TradingBot/src/services/api"
+	"strings"
+)
+
+// IsSessionDisconnectedError ...
+func (s *API) IsSessionDisconnectedError(err error) bool {
+	return err != nil && strings.Contains(err.Error(), SessionDisconnectedErrorString)
+}
+
+// IsOrderAlreadyExistsError ...
+func (s *API) IsOrderAlreadyExistsError(err error) bool {
+	return err != nil && strings.Contains(err.Error(), SessionDisconnectedErrorString)
+}
+
+// IsNotEnoughFundsError ...
+func (s *API) IsNotEnoughFundsError(err error) bool {
+	return err != nil && strings.Contains(err.Error(), NotEnoughFundsErrorString)
+}
+
+// IsOrderPendingCancelError ...
+func (s *API) IsOrderPendingCancelError(err error) bool {
+	return err != nil && strings.Contains(err.Error(), OrderIsPendingCancelErrorString)
+}
+
+// IsOrderCancelledError ...
+func (s *API) IsOrderCancelledError(err error) bool {
+	return err != nil && strings.Contains(err.Error(), OrderIsCancelledErrorString)
+}
+
+// IsOrderFilledError ...
+func (s *API) IsOrderFilledError(err error) bool {
+	return err != nil && strings.Contains(err.Error(), OrderIsFilledErrorString)
+}
+
+// IsInvalidHoursError ...
+func (s *API) IsInvalidHoursError(err error) bool {
+	return err != nil && strings.Contains(err.Error(), InvalidHoursErrorString)
+}
+
+// IsClosePositionRequestInProgressError ...
+func (s *API) IsClosePositionRequestInProgressError(err error) bool {
+	return err != nil && strings.Contains(err.Error(), ClosePositionRequestInProgressErrorString)
+}
+
+// IsLimitOrder ...
+func (s *API) IsLimitOrder(order *api.Order) bool {
+	return order.Type == LimitType
+}
+
+// IsStopOrder ...
+func (s *API) IsStopOrder(order *api.Order) bool {
+	return order.Type == StopType
+}
+
+// IsLongOrder ...
+func (s *API) IsLongOrder(order *api.Order) bool {
+	return order.Side == LongSide
+}
+
+// IsShortOrder ...
+func (s *API) IsShortOrder(order *api.Order) bool {
+	return order.Side == ShortSide
+}
+
+// IsLongPosition ...
+func (s *API) IsLongPosition(position *api.Position) bool {
+	return position.Side == LongSide
+}
+
+// IsShortPosition ...
+func (s *API) IsShortPosition(position *api.Position) bool {
+	return position.Side == ShortSide
+}
+
+// IsWorkingOrder ...
+func (s *API) IsWorkingOrder(order *api.Order) bool {
+	return order.Status == StatusWorkingOrder
+}
--- src/services/api/ibroker/ibroker.go
+++ src/services/api/ibroker/ibroker.go
@@ -13,7 +13,6 @@ import (
 	modifyposition "TradingBot/src/services/api/ibroker/modifyPosition"
 	"TradingBot/src/services/logger"
 	"errors"
-	"strings"
 
 	"encoding/json"
 	"net/http"
@@ -335,74 +334,6 @@ func (s *API) SetTimeout(t time.Duration) {
 // GetTimeout ...
 func (s *API) GetTimeout() time.Duration {
 	return s.timeout
-}
-
-// IsSessionDisconnectedError ...
-func (s *API) IsSessionDisconnectedError(err error) bool {
-	return err != nil && strings.Contains(err.Error(), SessionDisconnectedErrorString)
-}
-
-// IsOrderAlreadyExistsError ...
-func (s *API) IsOrderAlreadyExistsError(err error) bool {
-	return err != nil && strings.Contains(err.Error(), SessionDisconnectedErrorString)
-}
-
-// IsNotEnoughFundsError ...
-func (s *API) IsNotEnoughFundsError(err error) bool {
-	return err != nil && strings.Contains(err.Error(), NotEnoughFundsErrorString)
-}
-
-// IsOrderPendingCancelError ...
-func (s *API) IsOrderPendingCancelError(err error) bool {
-	return err != nil && strings.Contains(err.Error(), OrderIsPendingCancelErrorString)
-}
-
-// IsOrderCancelledError ...
-func (s *API) IsOrderCancelledError(err error) bool {
-	return err != nil && strings.Contains(err.Error(), OrderIsCancelledErrorString)
-}
-
-// IsOrderFilledError ...
-func (s *API) IsOrderFilledError(err error) bool {
-	return err != nil && strings.Contains(err.Error(), OrderIsFilledErrorString)
-}
-
-// IsInvalidHoursError ...
-func (s *API) IsInvalidHoursError(err error) bool {
-	return err != nil && strings.Contains(err.Error(), InvalidHoursErrorString)
-}
-
-// IsClosePositionRequestInProgressError ...
-func (s *API) IsClosePositionRequestInProgressError(err error) bool {
-	return err != nil && strings.Contains(err.Error(), ClosePositionRequestInProgressErrorString)
-}
-
-func (s *API) IsLimitOrder(order *api.Order) bool {
-	return order.Type == LimitType
-}
-
-func (s *API) IsStopOrder(order *api.Order) bool {
-	return order.Type == StopType
-}
-
-func (s *API) IsLongOrder(order *api.Order) bool {
-	return order.Side == LongSide
-}
-
-func (s *API) IsShortOrder(order *api.Order) bool {
-	return order.Side == ShortSide
-}
-
-func (s *API) IsLongPosition(position *api.Position) bool {
-	return position.Side == LongSide
-}
-
-func (s *API) IsShortPosition(position *api.Position) bool {
-	return position.Side == ShortSide
-}
-
-func (s *API) IsWorkingOrder(order *api.Order) bool {
-	return order.Status == StatusWorkingOrder
 }
 
 func (s *API) GetBracketOrdersForOpenedPosition(position *api.Position) (
