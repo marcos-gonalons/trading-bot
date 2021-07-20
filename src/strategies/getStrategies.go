a1d2ce0009f199adf0606c2757cdaa35a4097865
Marcos Gonalons <m.gonalons.camps@gmail.com>
2021-07-21 00:48:13 +0200

    No need to pass those params

--- src/strategies/getStrategies.go
+++ src/strategies/getStrategies.go
@@ -1,34 +1,23 @@
 package strategies
 
 import (
-	"TradingBot/src/services/api"
-	"TradingBot/src/services/api/retryFacade"
 	"TradingBot/src/services/candlesHandler"
-	"TradingBot/src/services/logger"
 	"TradingBot/src/services/technicalAnalysis/horizontalLevels"
 	"TradingBot/src/services/technicalAnalysis/trends"
 	"TradingBot/src/strategies/interfaces"
 	"TradingBot/src/strategies/tickers/GER30"
 )
 
-func (s *Handler) getStrategies(
-	API api.Interface,
-	APIRetryFacade retryFacade.Interface,
-	logger logger.Interface,
-) []interfaces.StrategyInterface {
+func (s *Handler) getStrategies() []interfaces.StrategyInterface {
 	return []interfaces.StrategyInterface{
-		s.getGER30Strategy(API, APIRetryFacade, logger),
+		s.getGER30Strategy(),
 	}
 }
 
-func (s *Handler) getGER30Strategy(
-	API api.Interface,
-	APIRetryFacade retryFacade.Interface,
-	logger logger.Interface,
-) interfaces.StrategyInterface {
-	GER30Strategy := GER30.GetStrategyInstance(API, APIRetryFacade, logger)
+func (s *Handler) getGER30Strategy() interfaces.StrategyInterface {
+	GER30Strategy := GER30.GetStrategyInstance(s.API, s.APIRetryFacade, s.Logger)
 	candlesHandler := &candlesHandler.Service{
-		Logger:    logger,
+		Logger:    s.Logger,
 		Symbol:    GER30Strategy.Parent().GetSymbol().BrokerAPIName,
 		Timeframe: *GER30Strategy.Parent().GetTimeframe(),
 	}
