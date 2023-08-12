094d03c67dc8d0da013d6d15a4b13de29f340ff5
Marcos Gonalons <m.gonalons.camps@gmail.com>
2023-08-12 10:47:11 +0200

    use 4h candles file for dax

--- src/markets/DAX/market.go
+++ src/markets/DAX/market.go
@@ -44,7 +44,7 @@ func GetMarketInstance() markets.MarketInterface {
 			Value: 4,
 			Unit:  "h",
 		},
-		CandlesFileName:        "DAX-15M.csv",
+		CandlesFileName:        "DAX-4H.csv",
 		EurExchangeRate:        1,
 		PositionSizeMultiplier: .5,
 		MinPositionSize:        1,
